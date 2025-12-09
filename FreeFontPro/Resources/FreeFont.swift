82db799ca758c37ecf9238bfc7af69ae992a58f5
qiuzefan <zf.qiu@giim.ac.cn>
2025-12-09 09:08:38 +0800

    优化字体信息

--- FreeFontPro/Resources/FreeFont.swift
+++ FreeFontPro/Resources/FreeFont.swift
@@ -57,13 +57,23 @@ let FreeFont: [FreeFontModel] = [
         languages: ["en"],
         names: ["42dot Sans"],
         descriptions: [
-          "42dot Sans is the corporate typeface for 42dot, designed to encapsulate the brand’s core identity and philosophy. With its harmonious balance of straight lines that represent cutting-edge technology and gentle curves that exude user-friendliness, 42dot Sans reflects the perfect synergy between precision and approachability.",
+"""
+Asta Sans is a modern, highly adaptable typeface designed for seamless use across a wide spectrum of applications. Taking inspiration from the asterisk (ASCII code 42, *), a symbol of universality and openness, Asta Sans embodies clarity, neutrality, and inclusiveness.
+
+With its harmonious balance of straight lines that represent cutting-edge technology and gentle curves that exude user-friendliness,
+
+Asta Sans reflects the perfect synergy between precision and approachability.
+
+The square-like interior shapes bring a sense of structure and organization, aligning seamlessly with the brand’s emphasis on clarity and functionality.
+
+This typeface is engineered for exceptional legibility across various contexts, whether in lengthy passages or concise text. The smooth flow of its lines ensures readability in diverse environments, making it an ideal choice for digital interfaces, print media, and beyond.
+""",
         ],
         weights: ["Regular"],
         license: "SIL Open Font License 1.1",
-        licenseUrl: "https://github.com/42dot/42dot-Sans/LICENSE-OFL",
+        licenseUrl: "https://github.com/42dot/Asta-Sans/blob/main/OFL.txt",
         website: "https://github.com/42dot/42dot-Sans",
-        copyright: "Copyright (c) 2023-2025, Astro_2539",
+        copyright: "Copyright (c) 2023-2025, 42dot",
         postscriptNames: [
             .init(
               language: "en",
